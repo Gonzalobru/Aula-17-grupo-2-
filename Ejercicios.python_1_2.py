f33071fb20ec4306cf138930fcf1c4213673e992
Gonzalobru <gonzalo.bruzzo@gmail.com>
2022-08-13 20:12:44 -0300

    Agrego los ejercicios 1 y 2 de python

---
+++ Ejercicios.python_1_2.py
@@ -0,0 +1,23 @@
+/*Ejercicios de Python*/
+
+
+1-
+
+dueno = [289577346,"Juan","Perez", "4789689","Belgrano 101" ]
+
+if dueno[0] > 26000000 :
+
+    print(dueno[3])
+
+
+2-
+
+dueno2 = [23546987,"Maria","Perez",4789689,"Pueyrredon 811"]
+i=0
+
+
+for i in range (len(dueno2)) :
+    if i != 0 and i != 2 :
+        print(dueno2[i])
+
+
